21f2e048245efe62d356106c47d6d4eb08f376c2
wa1YanLeung <siuhala@gmail.com>
2021-05-06 08:07:09 -0400

    change procfile

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,13 @@ if (process.env.NODE_ENV === "production") {
     })
 }
 
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('frontend/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//     })
+// }
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

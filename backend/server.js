6752c7ed6b46bc2a64f474800a338ef33c720312
wa1YanLeung <siuhala@gmail.com>
2021-05-06 13:13:20 -0400

    revise

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,25 @@ const app = express();
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send("Api running")
-    })
-}
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 //     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//         res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+//     })
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send("Api running")
 //     })
 // }
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
